7dedfa45c1f17c152bae83f547feadd0ccce7b6e
Mark Wu <wfwf1997@gmail.com>
2025-08-19 09:42:21 +0000

    Add sync icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { icons as syncIcons } from "./modules/sync";
+
 (async function main() {
-  const { icons } = await import("./modules/async");
-  console.log(icons);
+  console.log(syncIcons);
+  const { icons: asyncIcons } = await import("./modules/async");
+  console.log(asyncIcons);
 })();
---
+++ src/modules/sync.js
@@ -0,0 +1,3 @@
+import { AddFilled } from "@fluentui/react-icons";
+
+export const icons = { AddFilled };
